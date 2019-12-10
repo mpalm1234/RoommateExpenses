d92c0fc6cf1a2fc23bb18e4947532ac975ed5a88
Michelle Palmieri <michellepalmieri1@gmail.com>
2019-12-09 20:03:11 -0500

    cleaned up into functions

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,46 @@
 package roommateExpenses;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Calculator {
 	
-	public static Roommate buddy1;
-	public static Roommate buddy2;
+	public static Calculator calc;
+	public final int QTY = 3;
+	Roommate friend1, friend2;
 	
-	public Calculator(Roommate b1, Roommate b2) {
-		this.buddy1 = b1;
-		this.buddy2 = b2;
+	Scanner userInput = new Scanner(System.in);
+	
+	public Calculator() {
+		
+		// First roommate info
+		this.friend1 = prompt(1);
+		
+		// Second roommmate info
+		this.friend2 = prompt(2);
+		
+		// Calculate Difference
+		this.getPay(friend1, friend2);
+			
 	}
 	
-	public static void getPay(Roommate pal1, Roommate pal2) {
+	public Roommate prompt(int count) {
+		String name, type;
+		double paid;
+		Expense[] exps = new Expense[QTY];
+		
+		System.out.println(count + ": Enter roommate name:");
+		name = userInput.next();
+
+		System.out.println("Enter roommate's expenses:");
+		for(int i = 0; i < QTY; i++) {
+			type = "Electric";
+		    paid = userInput.nextDouble();
+		    exps[i] = new Expense(type, paid);
+		}
+		return new Roommate(name, exps);
+	}
+	
+	public void getPay(Roommate pal1, Roommate pal2) {
 		double diff, tot1 = 0, tot2 = 0;
 		
 		for(Expense e : pal1.getExps()) {
@@ -37,50 +64,7 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		
-		Scanner userInput = new Scanner(System.in);
-		
-		String name1, name2;
-		String type;
-		double paid;
-		
-		int qty = 3;
-		
-		Expense exp;
-		Expense[] exps1 = new Expense[qty];
-		Expense[] exps2 = new Expense[qty];
-
-		
-		// First roommmate info
-		System.out.println("Enter first roommate name:");
-		name1 = userInput.next();
-	
-		System.out.println("Enter first roommate's expenses:");
-		for(int i = 0; i < qty; i++) {
-			type = "Groceries";
-		    paid = userInput.nextDouble();
-		    exp = new Expense(type, paid);
-		    exps1[i] = exp;
-		}
-		Roommate friend1 = new Roommate(name1, exps1);
-		
-		// Second roommmate info
-		System.out.println("Enter second roommate name:");
-		name2 = userInput.next();
-
-		System.out.println("Enter second roommate's expenses:");
-		for(int i = 0; i < qty; i++) {
-			type = "Electric";
-		    paid = userInput.nextDouble();
-		    exp = new Expense(type, paid);
-		    exps2[i] = exp;
-		}
-		Roommate friend2 = new Roommate(name2, exps2);
-
-		
-		
-		// Calculate Difference
-		getPay(friend1, friend2);
-		
+		calc = new Calculator();
 		
 	}
 	
