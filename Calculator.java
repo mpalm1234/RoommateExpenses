64073141cf943196caf1378f3a8e611551e02a1f
Michelle Palmieri <michellepalmieri1@gmail.com>
2020-01-06 17:55:34 -0500

    Moved logic regarding JPanel to RenderPanel class, still need to figure out why key listener is not working

--- Calculator.java
+++ Calculator.java
@@ -1,150 +1,76 @@
 package roommateExpenses;
 
-import java.awt.*;
 import javax.swing.*;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.text.NumberFormat;
-import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class Calculator implements ActionListener, KeyListener{
 
-public class Calculator extends JPanel implements PropertyChangeListener{
-
-	public RenderPanel renderPanel;
-	public JFrame jframe;
-	public Dimension dim;
-	public NumberFormat amountFormat;
-
-	public JTextField rmname1, rmname2;
-	public JFormattedTextField rm1ex1, rm1ex2, rm1ex3, rm1ex4, rm1ex5;
-	public JFormattedTextField rm2ex1, rm2ex2, rm2ex3, rm2ex4, rm2ex5;
-
-
-	public static Calculator calc;
-	Roommate friend1, friend2;
-
-	Scanner userInput = new Scanner(System.in);
-
-	public static final int QTY = 3;
-	public static final int FRAMESIZE = 750;
-
+	public JFrame frame = new JFrame("Roommate Expenses Calculator");
+	public RenderPanel renderPanel = new RenderPanel();
+	
 	public Calculator() {
+		frame.add(renderPanel);
+		frame.setSize(renderPanel.FRAMEX, renderPanel.FRAMEY);
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+	}
 
-		super(new GridLayout());
-		amountFormat = NumberFormat.getNumberInstance();
-		displayFields();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		renderPanel.repaint();
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		String name;
+		double amount;
+		Expense[] exps;
 
-		// Calculate Expenses
-		//		this.friend1 = prompt(1);		// Get 1st roommate info
-		//		this.friend2 = prompt(2);		// Get 2nd roommate info
-		//		this.getPay(friend1, friend2);	// Calculate difference
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+
+			// Create Roommate #1
+			exps = null;
+			name = renderPanel.rmname1.getText();
+			for (int i = 0; i < renderPanel.MAXEXPS; i++) {
+				amount = (double) renderPanel.rm1exs[i].getValue();
+				exps[i] = new Expense(" ", amount);	
+			}
+			Roommate friend1 = new Roommate(name, exps);
+
+			// Create Roommate #2
+			exps = null;
+			name = renderPanel.rmname2.getText();
+			for (int i = 0; i < renderPanel.MAXEXPS; i++) {
+				amount = (double) renderPanel.rm2exs[i].getValue();
+				exps[i] = new Expense(" ", amount);	
+			}
+			Roommate friend2 = new Roommate(name, exps);
+
+			// Calculate Difference
+			renderPanel.owedStatement = getPay(friend1, friend2); 
+
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void displayFields() {
-		
-		// Name Fields
-		rmname1 = new JTextField(" ",15);
-		rmname1.addPropertyChangeListener("value", this);
-		
-		rmname2 = new JTextField(" ",15);
-		rmname2.addPropertyChangeListener("value", this);
-		
-		// Expense Fields
-		rm1ex1 = new JFormattedTextField(amountFormat);
-		rm1ex1.setValue(0);
-		rm1ex1.addPropertyChangeListener("value", this);
-		
-		rm1ex2 = new JFormattedTextField(amountFormat);
-		rm1ex2.setValue(0);
-		rm1ex2.addPropertyChangeListener("value", this);
-		
-		rm1ex3 = new JFormattedTextField(amountFormat);
-		rm1ex3.setValue(0);
-		rm1ex3.addPropertyChangeListener("value", this);
-		
-		rm1ex4 = new JFormattedTextField(amountFormat);
-		rm1ex4.setValue(0);
-		rm1ex4.addPropertyChangeListener("value", this);
-		
-		rm1ex5 = new JFormattedTextField(amountFormat);
-		rm1ex5.setValue(0);
-		rm1ex5.addPropertyChangeListener("value", this);
-		
-		rm2ex1 = new JFormattedTextField(amountFormat);
-		rm2ex1.setValue(0);
-		rm2ex1.addPropertyChangeListener("value", this);
-		
-		rm2ex2 = new JFormattedTextField(amountFormat);
-		rm2ex2.setValue(0);
-		rm2ex2.addPropertyChangeListener("value", this);
-		
-		rm2ex3 = new JFormattedTextField(amountFormat);
-		rm2ex3.setValue(0);
-		rm2ex3.addPropertyChangeListener("value", this);
-		
-		rm2ex4 = new JFormattedTextField(amountFormat);
-		rm2ex4.setValue(0);
-		rm2ex4.addPropertyChangeListener("value", this);
-		
-		rm2ex5 = new JFormattedTextField(amountFormat);
-		rm2ex5.setValue(0);
-		rm2ex5.addPropertyChangeListener("value", this);
-		
-		// Labels		
-		JLabel title1 = new JLabel("Roommate #1");
-		JLabel title2 = new JLabel("Roommate #2");
-		JLabel nameLabel = new JLabel("Name: ");
-		JLabel exLabel = new JLabel("Expenses: ");
-
-		// Panel Layout
-		JPanel labelPane = new JPanel(new GridLayout(2,1));
-		labelPane.add(nameLabel);
-		labelPane.add(exLabel);
-		
-		JPanel rm1Pane = new JPanel(new GridLayout(7,0));
-		rm1Pane.add(title1);
-		rm1Pane.add(rmname1);
-		rm1Pane.add(rm1ex1);
-		rm1Pane.add(rm1ex2);
-		rm1Pane.add(rm1ex3);
-		rm1Pane.add(rm1ex4);
-		rm1Pane.add(rm1ex5);
-		
-		JPanel rm2Pane = new JPanel(new GridLayout(7,0));
-		rm2Pane.add(title2);
-		rm2Pane.add(rmname2);
-		rm2Pane.add(rm2ex1);
-		rm2Pane.add(rm2ex2);
-		rm2Pane.add(rm2ex3);
-		rm2Pane.add(rm2ex4);
-		rm2Pane.add(rm2ex5);
-
-		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		add(labelPane, BorderLayout.WEST);
-		add(rm1Pane, BorderLayout.CENTER);
-		add(rm2Pane, BorderLayout.EAST);
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public Roommate prompt(int count) {
-		String name, type;
-		double paid;
-		Expense[] exps = new Expense[QTY];
-
-		System.out.println(count + ": Enter roommate name:");
-		name = userInput.next();
-
-//		System.out.println("Enter roommate's expenses:");
-//		for(int i = 0; i < QTY; i++) {
-//			type = "Electric";
-//			paid = userInput.nextDouble();
-//			exps[i] = new Expense(type, paid);
-//		}
-		return new Roommate(name, exps);
-	}
-
-	public void getPay(Roommate pal1, Roommate pal2) {
+	public String getPay(Roommate pal1, Roommate pal2) {
 		double diff, tot1 = 0, tot2 = 0;
 
 		for(Expense e : pal1.getExps()) {
@@ -156,38 +82,18 @@ public class Calculator extends JPanel implements PropertyChangeListener{
 		diff = tot1 - tot2;
 
 		if (diff == 0) {
-			System.out.println("Both roommates have paid the same amount.");
+			return "Both roommates have paid the same amount.";
 		} else if (diff > 0) {
-			System.out.println(pal2.name + " owes " + pal1.name + " $" + String.format( "%.2f", diff/2));
+			return pal2.name + " owes " + pal1.name + " $" + String.format("%.2f", diff/2);
 		} else {
-			System.out.println(pal1.name + " owes " + pal2.name + " $" + String.format( "%.2f", diff/-2));
+			return pal1.name + " owes " + pal2.name + " $" + String.format("%.2f", diff/-2);
 		}
 
 	}
 
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	private static void createAndShowGUI() {
-		JFrame frame = new JFrame("Roommate Expenses Calculator");
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new Calculator());
-		frame.pack();
-		frame.setVisible(true);
-    }
-
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {  
-				createAndShowGUI();
-			}
-		});
+		new Calculator();
 	}
-
 
 
 }
--- RenderPanel.java
+++ RenderPanel.java
@@ -1,23 +1,150 @@
 package roommateExpenses;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.text.NumberFormat;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class RenderPanel extends JPanel {
 
+	public static final int FRAMEX = 750, FRAMEY = 300, LABELX = 150, FIELDX = 300;
+	public static final int MAXEXPS = 6, MAXROWS = 9;
+	
+	public NumberFormat amountFormat = NumberFormat.getNumberInstance();
+
+	public JTextField rmname1 = new JTextField(" ",15);
+	public JTextField rmname2 = new JTextField(" ",15);
+	public JFormattedTextField[] rm1exs = new JFormattedTextField[6];
+	public JFormattedTextField[] rm2exs = new JFormattedTextField[6];
+	public JTextField rtnFld  = new JTextField(" ",15);
+	public String owedStatement = " ";
+
+	public RenderPanel() {
+		super(new BorderLayout());
+
+		// Labels 
+		JLabel title1 = new JLabel("Roommate #1");
+		JLabel title2 = new JLabel("Roommate #2");
+
+		JLabel blankLabel = new JLabel(" ");
+		JLabel nameLabel = new JLabel("Name:");
+		JLabel rentLabel = new JLabel("Rent:");
+		JLabel waterLabel = new JLabel("Water:");
+		JLabel electricLabel = new JLabel("Electric:");
+		JLabel cableLabel = new JLabel("Cable:");
+		JLabel groceriesLabel = new JLabel("Groceries:");
+		JLabel otherLabel = new JLabel("Other:");
+
+		// Expense Fields
+		double amount = 0;
+		for (int i = 0; i < MAXEXPS; i++) {
+			rm1exs[i] = new JFormattedTextField(amountFormat);
+			rm1exs[i].setValue(amount);
+
+			rm2exs[i] = new JFormattedTextField(amountFormat);
+			rm2exs[i].setValue(amount);
+		}
+
+		// Panel Layout
+		JPanel labelPane = new JPanel(new GridLayout(MAXROWS, 0));
+		labelPane.setPreferredSize(new Dimension(LABELX, FRAMEY));
+		labelPane.add(blankLabel);
+		labelPane.add(nameLabel);
+		labelPane.add(rentLabel);
+		labelPane.add(waterLabel);
+		labelPane.add(electricLabel);
+		labelPane.add(cableLabel);
+		labelPane.add(groceriesLabel);
+		labelPane.add(otherLabel);
+
+		JPanel rm1Pane = new JPanel(new GridLayout(MAXROWS, 0));
+		rm1Pane.setPreferredSize(new Dimension(FIELDX, FRAMEY));
+		rm1Pane.add(title1);
+		rm1Pane.add(rmname1);
+		for (int i = 0; i < MAXEXPS; i++) {
+			rm1Pane.add(rm1exs[i]);
+		}
+
+		JPanel rm2Pane = new JPanel(new GridLayout(MAXROWS, 0));
+		rm2Pane.setPreferredSize(new Dimension(FIELDX, FRAMEY));
+		rm2Pane.add(title2);
+		rm2Pane.add(rmname2);
+		for (int i = 0; i < MAXEXPS; i++) {
+			rm2Pane.add(rm2exs[i]);
+		}
+
+		JPanel rtnPane = new JPanel(new GridLayout(1, 0));
+		rtnPane.add(rtnFld);
+
+		this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		this.add(labelPane, BorderLayout.WEST);
+		this.add(rm1Pane, BorderLayout.CENTER);
+		this.add(rm2Pane, BorderLayout.EAST);
+		this.add(rtnPane, BorderLayout.SOUTH);
+	}
+
+	public JTextField getRmname1() {
+		return rmname1;
+	}
+
+	public void setRmname1(JTextField rmname1) {
+		this.rmname1 = rmname1;
+	}
+
+	public JTextField getRmname2() {
+		return rmname2;
+	}
+
+	public void setRmname2(JTextField rmname2) {
+		this.rmname2 = rmname2;
+	}
+
+	public JFormattedTextField[] getRm1exs() {
+		return rm1exs;
+	}
+
+	public void setRm1exs(JFormattedTextField[] rm1exs) {
+		this.rm1exs = rm1exs;
+	}
+
+	public JFormattedTextField[] getRm2exs() {
+		return rm2exs;
+	}
+
+	public void setRm2exs(JFormattedTextField[] rm2exs) {
+		this.rm2exs = rm2exs;
+	}
+
+	public JTextField getRtnFld() {
+		return rtnFld;
+	}
+
+	public void setRtnFld(JTextField rtnFld) {
+		this.rtnFld = rtnFld;
+	}
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-//		Calculator calc = Calculator.calc;
 
 		//Background
-		g.setColor(Color.DARK_GRAY);
-		g.fillRect(0, 0, Calculator.FRAMESIZE, Calculator.FRAMESIZE);
+//		g.setColor(Color.DARK_GRAY);
+//		g.fillRect(0, 0, FRAMEX, FRAMEY);
 
 	}
+
+
+
+
 }
 
 
